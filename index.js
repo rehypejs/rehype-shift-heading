0cc74f9c36c881f219ff2bbf0b04efa420c07597
Titus Wormer <tituswormer@gmail.com>
2023-08-26 13:48:08 +0200

    Refactor to move implementation to `lib/`

--- index.js
+++ index.js
@@ -1,25 +1,5 @@
 /**
- * @typedef {import('hast').Root} Root
- *
- * @typedef Options
- *   Configuration.
- * @property {number} shift
- *   Number to shift headings.
+ * @typedef {import('./lib/index.js').Options} Options
  */
 
-import {shiftHeading} from 'hast-util-shift-heading'
-
-/**
- * Plugin to change the rank (depth, level) of headings.
- *
- * @type {import('unified').Plugin<[Options], Root>}
- */
-export default function rehypeShiftHeading(options) {
-  if (!options || !options.shift) {
-    throw new Error('Missing required `shift` in options')
-  }
-
-  return (tree) => {
-    shiftHeading(tree, options.shift)
-  }
-}
+export {default} from './lib/index.js'
