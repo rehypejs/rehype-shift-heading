070ad4493df649fe36f4677d7f749c3c69c3fa0e
Titus Wormer <tituswormer@gmail.com>
2023-08-26 14:07:53 +0200

    Change to use `exports`

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 import assert from 'node:assert/strict'
 import test from 'node:test'
 import {rehype} from 'rehype'
-import rehypeShiftHeading from './index.js'
+import rehypeShiftHeading from 'rehype-shift-heading'
 
 test('rehypeShiftHeading', async function (t) {
   await t.test('should expose the public api', async function () {
-    assert.deepEqual(Object.keys(await import('./index.js')).sort(), [
+    assert.deepEqual(Object.keys(await import('rehype-shift-heading')).sort(), [
       'default'
     ])
   })
